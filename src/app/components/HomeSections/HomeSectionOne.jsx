04952dfadb1e51c3c9a27debf60e6338c9bf5e95
Oghenevwede Louis Umukoro <wedevilleg@gmail.com>
2024-09-10 18:50:47 +0100

    updated HomeSectionOne

--- src/app/components/HomeSections/HomeSectionOne.jsx
+++ src/app/components/HomeSections/HomeSectionOne.jsx
@@ -10,9 +10,29 @@ import { LuSwitchCamera } from "react-icons/lu";
 import { MdOutlineSpeakerNotes } from "react-icons/md";
 import { LuRefreshCcw } from "react-icons/lu";
 import { MdOutlineFlip } from "react-icons/md";
+import { RiEyeCloseLine } from "react-icons/ri";
 
 
 const HomeSectionOne=()=>{
+
+    const [showAccountBalance, setShowAccountBalance]=useState(true);
+    const [showAccountBalance2, setShowAccountBalance2]=useState(true);
+    const [showAccountBalance3, setShowAccountBalance3]=useState(true);
+
+    const toggleShowAccountBalance=()=>{
+        setShowAccountBalance(!showAccountBalance)
+        // setShowAccountBalance(false)
+    }
+
+    const toggleShowAccountBalance2=()=>{
+        setShowAccountBalance2(!showAccountBalance2)
+    }
+
+    const toggleShowAccountBalance3=()=>{
+        setShowAccountBalance3(!showAccountBalance3)
+    }
+
+
 
     return(
         <>
@@ -22,22 +42,26 @@ const HomeSectionOne=()=>{
                     {/* Personal Account Div */}
                     <div className="bg-white w-fit px-[1rem] py-[1rem] rounded-[8px]">
                         <div className="flex items-center  ">
-                            <h1>Personal Account</h1>
-                            <button className="ml-[8rem]">+ Add Card</button>
+                            <h1 className="text-sm">Personal Account</h1>
+                            <button className="ml-[8rem] text-sm border border-black rounded-[5px] py-[3px] px-2">+ Add Card</button>
                         </div>
 
-                        <div className="flex items-center">
-                            <h1>$0.00</h1>
-                            <p className="ml-auto"><VscEye /></p>
+                        <div className="flex items-center mt-5 font-bold">
+                            <h1 className="text-xl">$0.00</h1>
+        
+                            <div onClick={toggleShowAccountBalance} className="flex items-center ml-auto">
+                                <p className="ml-auto cursor-pointer text-black text-xl"><VscEye /></p>
+                                <p className="ml-auto cursor-pointer text-black text-xl"><RiEyeCloseLine /></p> 
+                            </div>
                         </div>
 
                         {/* modals */}
-                        <div className="flex items-center justify-between gap-5">
+                        <div className="flex items-center justify-between gap-5 mt-5">
                             {/* 1 */}
                             <div>
                                 {/* You can open the modal using document.getElementById('ID').showModal() method */}
                                 <button className="bg-neutral-100 text-xl py-3 px-3 rounded-full" onClick={()=>document.getElementById('my_modal_3').showModal()}><BsSend /></button>
-                                <p>Send</p>
+                                <p className="text-sm">Send</p>
                                 <dialog id="my_modal_3" className="modal">
                                 <div className="modal-box">
                                 <form method="dialog">
@@ -54,14 +78,14 @@ const HomeSectionOne=()=>{
                             <div>
                                 {/* You can open the modal using document.getElementById('ID').showModal() method */}
                                 <button className="bg-neutral-100 text-xl py-3 px-3 rounded-full" onClick={()=>document.getElementById('my_modal_3').showModal()}><BsArrowDown /></button>
-                                <p>Receive</p>
+                                <p className="text-sm">Receive</p>
                                 <dialog id="my_modal_3" className="modal">
                                 <div className="modal-box">
                                 <form method="dialog">
                                 {/* if there is a button in form, it will close the modal */}
                                 <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                                 </form>
-                                <h3 className="font-bold text-lg">Transfer</h3>
+                                <h3 className="font-bold text-lg">Receive</h3>
                                 <p className="py-4">Press ESC key or click on ✕ button to close</p>
                                 </div>
                                 </dialog>
@@ -71,14 +95,14 @@ const HomeSectionOne=()=>{
                             <div>
                                 {/* You can open the modal using document.getElementById('ID').showModal() method */}
                                 <button className="bg-neutral-100 text-xl py-3 px-3 rounded-full" onClick={()=>document.getElementById('my_modal_3').showModal()}><LuSwitchCamera /></button>
-                                <p>Withdraw</p>
+                                <p className="text-sm">Withdraw</p>
                                 <dialog id="my_modal_3" className="modal">
                                 <div className="modal-box">
                                 <form method="dialog">
                                 {/* if there is a button in form, it will close the modal */}
                                 <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                                 </form>
-                                <h3 className="font-bold text-lg">Transfer</h3>
+                                <h3 className="font-bold text-lg">Withdraw</h3>
                                 <p className="py-4">Press ESC key or click on ✕ button to close</p>
                                 </div>
                                 </dialog>
@@ -88,22 +112,22 @@ const HomeSectionOne=()=>{
                             <div>
                                 {/* You can open the modal using document.getElementById('ID').showModal() method */}
                                 <button className="bg-neutral-100 text-xl py-3 px-3 rounded-full" onClick={()=>document.getElementById('my_modal_3').showModal()}><MdOutlineSpeakerNotes /></button>
-                                <p>Pay Bill</p>
+                                <p className="text-sm">Pay Bill</p>
                                 <dialog id="my_modal_3" className="modal">
                                 <div className="modal-box">
                                 <form method="dialog">
                                 {/* if there is a button in form, it will close the modal */}
                                 <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                                 </form>
-                                <h3 className="font-bold text-lg">Transfer</h3>
+                                <h3 className="font-bold text-lg"></h3>
                                 <p className="py-4">Press ESC key or click on ✕ button to close</p>
                                 </div>
                                 </dialog>
                             </div>
                         </div>
 
-                        <div className="flex items-center">
-                            <p>Last Updated: Just Now </p>
+                        <div className="flex items-center mt-5 text-neutral-400">
+                            <p className="text-sm">Last Updated: Just Now </p>
                             <p className="ml-auto"><LuRefreshCcw /></p>
 
                         </div>
@@ -111,36 +135,61 @@ const HomeSectionOne=()=>{
 
 
                     {/* Card Div */}
-                    <div className="bg-blue-400 text-white w-fit px-[1rem] py-[1rem] rounded-[8px]">
+                    <div className="bg-gradient-to-r from-cyan-500 to-cyan-900 text-white w-fit px-[1rem] py-[1rem] rounded-[8px]">
                         <div className="flex items-center">
-                            <div className="flex items-center gap-1 border ">
-                                <p><MdOutlineFlip /></p>
-                                <p>Flip Card</p>
+                            <div className="flex items-center gap-1 border font-semibold border-white rounded-[5px] py-[3px] px-2 ">
+                                <p className=""><MdOutlineFlip /></p>
+                                <p className="text-sm">Flip Card</p>
                             </div>
-                            <div>logo</div>
+                            <div className="ml-[12rem] font-extrabold text-lg">Co-Bank</div>
                         </div>
 
-                        <div>
-                            <p>Balance</p>
-                            <div className="flex items-center">
-                                <p>$2,000,000.00</p>
-                                <p><VscEye /></p>
+                        <div className="mt-5">
+                            <p className="text-sm font-semibold">Balance</p>
+                            <div onClick={toggleShowAccountBalance2} className="flex items-center cursor-pointer">
+
+                                {showAccountBalance2 ? (
+                                
+                                <>
+                                    <p className="font-bold text-2xl ">$2,000,000.00</p>
+                                    <p className="ml-10 text-xl "><VscEye /></p>
+                                </>)
+
+                                : (
+
+                                <>
+                                    <p className="font-bold text-2xl bg-opacity-100  blurry-text ">$0,000,000.00</p>
+                                    <p className=" cursor-pointer ml-10 text-xl"><RiEyeCloseLine /></p>
+                                </>)}
+                                
+                                
                             </div>
                         </div>
 
-                        <div className="flex items-center">
-                            <p>1234 1234 1234 1234</p>
-                            <p><VscEye /></p>
+                        <div onClick={toggleShowAccountBalance3} className="flex items-center mt-5 cursor-pointer">
+
+                            {showAccountBalance3 ? (
+                                <>
+                                    <p className="font-bold ">1234 1234 1234 1234</p>
+                                    <p className="ml-auto text-xl"><VscEye /></p>
+                                </> )
+
+                                : (
+                                    <>
+                                        <p className="font-bold bg-opacity-100  blurry-text text-2xl">0000 0000 0000 0000</p>
+                                        <p className=" cursor-pointer ml-auto text-xl"><RiEyeCloseLine /></p>
+                                    </>)}
+
                         </div>
 
-                        <div className="flex items-center">
-                            <p>Louis Umukoro</p>
+                        <div className="flex items-center justify-between mt-5">
+                            <p className="font-bold">Louis Umukoro</p>
                             <div>
-                                <p>Expires</p>
-                                <p>07/26</p>
+                                <p className="text-sm">Expires</p>
+                                <p className="font-bold">07/26</p>
                             </div>
 
-                            <p>VISA</p>
+                            <p className="text-xl font-bold">VISA</p>
                         </div>
                     </div>
                 </div>
