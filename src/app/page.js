e1c2e9577fbf31d3d80bd901e73b1f18208a35a6
Zuriel <chiekezuriel2020@gmail.com>
2024-09-16 08:58:46 +0100

    my second target commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,7 @@
 'use client'
-
-
 import Navbar from "./components/Navbar";
 import HomeSectionOne from "./components/HomeSections/HomeSectionOne";
+import Target from "./components/Target";
 
 export default function Home() {
   return (
@@ -11,6 +10,7 @@ export default function Home() {
           
           <Navbar/>
           <HomeSectionOne/>
+          <Target/>
         </div>
       </>
   );
